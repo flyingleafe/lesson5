3d5b9f00f392cdc9aa9bde31aadeed3cd10599db
flyingleafe <flyingleafe@gmail.com>
2014-10-21 02:40:27 +0400

    change some names

--- app/src/main/java/com/dreamteam/app/exercise1/FeedParser.java
+++ app/src/main/java/com/dreamteam/app/exercise1/FeedParser.java
@@ -2,7 +2,7 @@ package com.dreamteam.app.exercise1;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
+
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
